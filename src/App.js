6e835114e6fe0b61235d9e25d6646523a6bed366
pmusole2 <pmusole2@gmail.com>
2020-04-16 00:31:12 +0200

    Collection Component Added

--- src/App.js
+++ src/App.js
@@ -2,24 +2,17 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import { HomePage } from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
-
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/hats' component={HatsPage} />
-        </Switch>
-      </Router>
-    );
-  }
-}
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
